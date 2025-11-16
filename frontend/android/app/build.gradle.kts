154b94f5b027d560277c2aff04fb26ef1d369a39
Dmitry Olegovich Sorokin (@sorydima) <43252008+sorydima@users.noreply.github.com>
2025-11-16 20:46:12 +0300

    Update build.gradle.kts

--- frontend/android/app/build.gradle.kts
+++ frontend/android/app/build.gradle.kts
@@ -67,7 +67,7 @@ flutter {
 dependencies {
     coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.4")
     
-    // Play Core for split compatibility - временно отключаем
-    // implementation("com.google.android.play:core:1.10.3")
-    // implementation("com.google.android.play:core-ktx:1.8.1")
+    // Play Core for split compatibility
+    implementation("com.google.android.play:core:1.10.3")
+    implementation("com.google.android.play:core-ktx:1.8.1")
 }
